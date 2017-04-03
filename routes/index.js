0f8d8a943493b0c2825a39ca47bddcb981651442
henri giordano <henri giordano>
2017-04-04 00:48:20 +0200

    ver9

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,7 @@ var db;
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-	mongoenv=process.env.OPENSHIFT_MONGODB_DB_URL;
-	console.log (mongoenv);
+	
 
     mongoURLLabel = "";
   db = mongoose.createConnection(mongoenv, 'pollsapp');
@@ -34,7 +33,7 @@ console.log ( 'henri2 :' +mongoURLLabel);
 
 //if (process.env.OPENSHIFT_MONGODB_DB_URL) {
  //  db = mongoose.connect(mongoURLLabel,{user: mongoUser, pass: mongoPassword });
-//   db = mongoose.createConnection(mongoURLLabel, 'pollsapp');
+  db = mongoose.createConnection(mongoURL,{uri_decode_auth: true}, 'pollsapp');
 //} else {
  //  db = mongoose.createConnection('localhost', 'pollsapp');
 //}
